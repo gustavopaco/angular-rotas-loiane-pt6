3c22421ac7589f1f9cbe5982f437490a449f6e32
Gustavo Paco <gustavopaco@gmail.com>
2021-09-02 10:11:00 -0300

    - Refatorando codigo e construindo sub-Modulo de funcionalidade de Cursos como exemplo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-// import {routing} from "./app.routing";
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import {FormsModule} from "@angular/forms";
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import {AppRoutingModule} from "./app.routing.module";
+import {CursosModule} from "./cursos/cursos.module";
+// import {routing} from "./app.routing";
+//import { CursosComponent } from './cursos/cursos.component';
+//import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+//import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    LoginComponent
+    // CursosComponent,
+    // CursoDetalheComponent,
+    // CursoNaoEncontradoComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
+    CursosModule
     // routing,
   ],
   providers: [],
---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CursosComponent} from "./cursos.component";
+import {CursoDetalheComponent} from "./curso-detalhe/curso-detalhe.component";
+import {CursoNaoEncontradoComponent} from "./curso-nao-encontrado/curso-nao-encontrado.component";
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+
+
+@NgModule({
+  declarations: [
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ],
+  exports: [
+  ]
+})
+export class CursosModule {
+}
