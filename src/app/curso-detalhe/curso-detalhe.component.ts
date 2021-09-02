7d4b8a4dbd0757d462962fc65a7bbc0fb1670546
Gustavo Paco <gustavopaco@gmail.com>
2021-09-02 07:37:22 -0300

    - ***IMPORTANT*** - Recuperar parametro do Path da URL "usuario/1". Devemos injetar a classe ActivatedRoute para detectar quando a rota esta ativa e para recuperar o parametro, utilizar: -> this.route.params.subscribe((response: any) => this.id = response['id'])
    - Devemos tambem criar uma variavel do tipo Subscribe para receber o subscribe da Rota e por fim implementar a Interface OnDestroy e dar unsubscribe como BOA PRATICA

--- src/app/curso-detalhe/curso-detalhe.component.ts
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
   styleUrls: ['./curso-detalhe.component.scss']
 })
-export class CursoDetalheComponent implements OnInit {
+export class CursoDetalheComponent implements OnInit, OnDestroy {
 
   id: string | null = "";
+  inscricao: Subscription;
 
   /* IMPORTANT: ActivatedRoute, responsavel por detectar quando a rota desse componente esta ativa e receber parametros via Path na URL => usuario/1 */
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {
+    this.inscricao = new Subscription();
+  }
 
   ngOnInit(): void {
     // this.id = this.route.snapshot.params['id'];
-    this.id = this.route.snapshot.paramMap.get("id");
+    // this.id = this.route.snapshot.paramMap.get("id");
     // this.id = this.route.snapshot.params.id;
+    this.inscricao = this.route.params.subscribe((response : any) => this.id = response["id"])
   }
 
+  /* IMPORTANT: Como boa pratica, deve-se criar uma variavel do tipo Subscription para receber a chamada do subscribe (de um servico por exemplo)
+        e implementar a interface do componente (*OnDestroy), e remover a inscricao*/
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe()
+  }
 }
