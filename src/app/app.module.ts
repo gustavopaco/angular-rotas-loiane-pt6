04bfa28c9a369f5ec41e28b3098afb14eb48cb98
Gustavo Paco <gustavopaco@gmail.com>
2021-09-02 06:07:48 -0300

    - Criando arquivo de routing e configurando URIs principais do sistema /home, /login, /cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import {routing} from "./app.routing";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
   ],
   imports: [
+    routing,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/app.routing.ts
@@ -0,0 +1,13 @@
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {CursosComponent} from "./cursos/cursos.component";
+import {ModuleWithProviders} from "@angular/core";
+
+const AppRouting: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "login", component: LoginComponent},
+  {path: "cursos", component: CursosComponent}
+];
+
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(AppRouting);
