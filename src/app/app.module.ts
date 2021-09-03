822283ed7bdaa2714836e9bd48e929dcec9d7e94
Gustavo Paco <gustavopaco@gmail.com>
2021-09-03 09:28:48 -0300

    - ***IMPORTANT -> Criando component para pagina nao encontrada.
    -> Obs: Por default existe uma pagina especifica para pagina nao encontrada, porem se quiser que redirecione para /login por exemplo, Remover comentario no app.routing.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import {FormsModule} from "@angular/forms";
 import {AppRoutingModule} from "./app.routing.module";
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 // import {AlunosModule} from "./alunos/alunos.module";
 // import {CursosModule} from "./cursos/cursos.module";
 // import {routing} from "./app.routing";
@@ -17,7 +18,8 @@ import {AppRoutingModule} from "./app.routing.module";
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    PaginaNaoEncontradaComponent
     // CursosComponent,
     // CursoDetalheComponent,
     // CursoNaoEncontradoComponent
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,6 +4,7 @@ import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {AuthGuard} from "./guards/auth.guard";
 import {CursosGuard} from "./guards/cursos.guard";
+import {PaginaNaoEncontradaComponent} from "./pagina-nao-encontrada/pagina-nao-encontrada.component";
 // import {AlunosGuard} from "./guards/alunos.guard";
 // import {AlunosComponent} from "./alunos/alunos.component";
 // import {CursosComponent} from "./cursos/cursos.component";
@@ -15,7 +16,9 @@ const appRoutes: Routes = [
   {path: "alunos", loadChildren: () => import("./alunos/alunos.module").then(mod => mod.AlunosModule), canActivate: [AuthGuard], canLoad : [AuthGuard]},
   {path: "login", component: LoginComponent},
   {path: "", component: HomeComponent, canActivate:[AuthGuard], canLoad : [AuthGuard]},
-  {path: "**", component: HomeComponent, canActivate:[AuthGuard], canLoad : [AuthGuard]}
+  {path: "**", component: PaginaNaoEncontradaComponent /*, canActivate:[AuthGuard], canLoad : [AuthGuard]
+    IMPORTANT: Descomentar *(canActivate e canLoad) acima, caso queira que ao tentar acessar URL nao existente redirecione para /login */
+  }
   // {path: "cursos", component: CursosComponent},
   // {path: "curso/:id", component: CursoDetalheComponent},
   // {path: "naoEncontrado", component: CursoNaoEncontradoComponent}
